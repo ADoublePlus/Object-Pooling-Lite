d57a2c242a977f5fda0dd6d1314e5623b8cfda20
John Wonson <jmpwonson@gmail.com>
2021-01-07 16:25:39 +1100

    Create DefaultPoolPolicy.cs

---
+++ ObjectPool/ObjectPool/DefaultPoolPolicy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace object_pooling_lite
+{
+    /// <summary>
+    /// Provides configuration controlling how an object pool works.
+    /// Will call empty constructor for creation of object.
+    /// </summary>
+    public class DefaultPoolPolicy<T> : IPoolPolicy<T> where T : class, new()
+    {
+        private readonly Action<T> _reinitializeObject = null;
+
+        public int MaximumPoolSize { get; }
+
+        public DefaultPoolPolicy(Action<T> reinitializeObject = null, int maxPoolSize = 10)
+        {
+            _reinitializeObject = reinitializeObject;
+            MaximumPoolSize = maxPoolSize > 0 ? maxPoolSize : Environment.ProcessorCount * 2;
+        }
+
+        public T Create(IPool<T> pool)
+        {
+#pragma warning disable HAA0502 // Explicit new reference type allocation
+            return new T();
+#pragma warning disable HAA0502
+        }
+
+        public void Reinitialize(T obj)
+        {
+            _reinitializeObject?.Invoke(obj);
+        }
+    }
+}
