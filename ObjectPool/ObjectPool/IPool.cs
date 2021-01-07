5ec570e513dd6be26ee4dae63706f78d0b00cd87
John Wonson <jmpwonson@gmail.com>
2021-01-07 16:14:39 +1100

    Create IPool.cs

---
+++ ObjectPool/ObjectPool/IPool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace object_pooling_lite
+{
+    public interface IPool<T> : IDisposable
+    {
+        /// <summary>
+        /// Gets an item from the pool.
+        /// </summary>
+        T Get();
+
+        /// <summary>
+        /// Gets an <see cref="PooledObject{T}"/> from the pool.
+        /// </summary>
+        PooledObject<T> GetPooledObject();
+
+        /// <summary>
+        /// Returns/adds an object to the pool so it can be reused.
+        /// </summary>
+        bool Return(T value);
+
+        /// <summary>
+        /// Returns true of the <see cref="IDisposable.Dispose"/> method has been called on this instance.
+        /// </summary>
+        bool IsDisposed { get; }
+    }
+}
